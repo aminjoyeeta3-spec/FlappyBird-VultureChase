443c5baaf378d0bbe8a24f95d127476acf35cd10
Nuzhat Nawar <nuzhatnawar08@gmail.com>
2026-09-03 01:44:25 +0600

    render: draw active coins in a loop; fix bird texture path

--- src/renderer/render.c
+++ src/renderer/render.c
@@ -1,16 +1,25 @@
 #include "raylib.h"
 #include "../../include/bird.h"
 #include "../../include/render.h"
+#include "../../include/coin.h"
 
 Texture2D birdTexture;
 
 void initRenderer(void){
-    birdTexture = LoadTexture("Picture/Assets/bird.png");
+    birdTexture = LoadTexture("assets/bird.png");
 }
 
 void renderFrame(void){
     BeginDrawing();
     ClearBackground(RAYWHITE);
+
     DrawTexture(birdTexture, bird.x, bird.y, WHITE);
+
+    for(int i = 0; i < MAX_COINS; i++){
+        if(coins[i].active == 1){
+            DrawRectangle(coins[i].x, coins[i].y, coins[i].width, coins[i].height, YELLOW);
+        }
+    }
+
     EndDrawing();
 }
