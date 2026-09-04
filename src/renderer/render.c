12d099a761a9da8a90a7b2df35bb87c3d0cb7d11
Nuzhat Nawar <nuzhatnawar08@gmail.com>
2026-09-05 01:31:14 +0600

    render: load coin/star textures, draw coins by type, bird image update

--- src/renderer/render.c
+++ src/renderer/render.c
@@ -4,9 +4,13 @@
 #include "../../include/coin.h"
 
 Texture2D birdTexture;
+Texture2D coinTexture;
+Texture2D starTexture;
 
 void initRenderer(void){
     birdTexture = LoadTexture("assets/bird.png");
+    coinTexture = LoadTexture("assets/coin.png");
+    starTexture = LoadTexture("assets/star.png");
 }
 
 void renderFrame(void){
@@ -17,7 +21,11 @@ void renderFrame(void){
 
     for(int i = 0; i < MAX_COINS; i++){
         if(coins[i].active == 1){
-            DrawRectangle(coins[i].x, coins[i].y, coins[i].width, coins[i].height, YELLOW);
+            if(coins[i].type == COIN){
+                DrawTexture(coinTexture, coins[i].x, coins[i].y, WHITE);
+            } else if(coins[i].type == GEM){
+                DrawTexture(starTexture, coins[i].x, coins[i].y, WHITE);
+            }
         }
     }
 
