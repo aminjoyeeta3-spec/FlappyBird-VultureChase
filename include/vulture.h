e38e3af2a228c94beaf0dbd0c4562f1d1985aad5
Joyeeta___ <aminjoyeeta3@gmail.com>
2026-09-08 19:48:46 +0600

    implement vulture gameplay logic

--- include/vulture.h
+++ include/vulture.h
@@ -3,8 +3,22 @@
 
 #include "config.h"
 
-typedef struct {
-    int active;
+#define VULTURE_STRIKE_TIME 5.0f
+
+#define VULTURE_WIDTH  40
+#define VULTURE_HEIGHT 40
+
+#define VULTURE_OFFSET 30
+
+typedef enum
+{
+    VULTURE_INACTIVE = 0,
+    VULTURE_ACTIVE   = 1
+} VultureState;
+
+typedef struct
+{
+    VultureState state;
     float timer;
     int x;
     int y;
@@ -14,8 +28,14 @@ typedef struct {
 
 void initVulture(Vulture *vulture);
 
-void activateVulture(Vulture *vulture);
+void activateVulture(Vulture *vulture, int birdX, int birdY);
 
-void updateVulture(Vulture *vulture, float deltaTime, int birdX, int birdY);
+void updateVulture(Vulture *vulture, float deltaTime);
+
+void deactivateVulture(Vulture *vulture);
+
+int isVultureActive(Vulture *vulture);
+
+int isVultureTimeUp(Vulture *vulture);
 
 #endif
--- src/gameplay/vulture.c
+++ src/gameplay/vulture.c
@@ -1,35 +1,71 @@
 #include "../../include/vulture.h"
+#include "../../include/safezone.h"
 
-void initVulture(Vulture *vulture) {
-    vulture->active = 0;
-    vulture->timer = 5.0f;
-    
-    vulture->width = 40;
-    vulture->height = 40;
-    
+void initVulture(Vulture *vulture)
+{
+    vulture->state  = VULTURE_INACTIVE;
+    vulture->timer  = 0.0f;
+    vulture->width  = VULTURE_WIDTH;
+    vulture->height = VULTURE_HEIGHT;
     vulture->x = -100;
     vulture->y = -100;
 }
 
-void activateVulture(Vulture *vulture) {
-    vulture->active = 1;
-    vulture->timer = 5.0f;
-}
+void activateVulture(Vulture *vulture, int birdX, int birdY)
+{
+    if (vulture->state == VULTURE_ACTIVE)
+    {
+        return;
+    }
 
-void updateVulture(Vulture *vulture, float deltaTime, int birdX, int birdY) {
+    vulture->state = VULTURE_ACTIVE;
+    vulture->timer = VULTURE_STRIKE_TIME;
+    vulture->x = birdX;
 
-    if (vulture->active == 1) {
-        
-        vulture->timer -= deltaTime;
-        
-        vulture->x = birdX - 60; 
-        
-        vulture->y = birdY;
-        
-        if (vulture->timer <= 0.0f) {
-            vulture->active = 0;
-            vulture->timer = 0.0f;
-            vulture->x = -100; 
+    if (birdY < safeZone.top)
+    {
+        vulture->y = safeZone.top - VULTURE_OFFSET - vulture->height;
+        if (vulture->y < 0)
+        {
+            vulture->y = 0;
         }
     }
+    else
+    {
+        vulture->y = safeZone.bottom + VULTURE_OFFSET;
+        if (vulture->y + vulture->height > SCREEN_HEIGHT)
+        {
+            vulture->y = SCREEN_HEIGHT - vulture->height;
+        }
+    }
+}
+
+void updateVulture(Vulture *vulture, float deltaTime)
+{
+    if (vulture->state == VULTURE_ACTIVE)
+    {
+        vulture->timer -= deltaTime;
+        if (vulture->timer < 0.0f)
+        {
+            vulture->timer = 0.0f;
+        }
+    }
+}
+
+void deactivateVulture(Vulture *vulture)
+{
+    vulture->state = VULTURE_INACTIVE;
+    vulture->timer = 0.0f;
+    vulture->x = -100;
+    vulture->y = -100;
+}
+
+int isVultureActive(Vulture *vulture)
+{
+    return vulture->state == VULTURE_ACTIVE;
+}
+
+int isVultureTimeUp(Vulture *vulture)
+{
+    return vulture->state == VULTURE_ACTIVE && vulture->timer <= 0.0f;
 }
